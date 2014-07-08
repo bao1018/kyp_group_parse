220071c5042e0fe9e63d3884e7f955bfff6d042e
Jeremy <jeremy.bao@cnsdc01.pwc.com>
2014-07-08 17:34:47 +0800

    add

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ var groupCol = ['groupId', 'name', 'repId', 'groupType', 'groupTypeDesc', 'SIC',
 
 var subGroupCol = ['subgroupId', 'SIC', 'SICDesc', 'effectiveDate', 'fcstType', 'fcstTypeDesc', 'groupType', 'groupTypeDesc', 'repId', 'subgroupName', 'contract_contractId', 'contract_carrierId', , 'contract_effectiveDate', 'contract_baseBenefitId', 'contract_baseBenefitDesc', 'contract_rateType', 'contract_serviceArea', 'contract_eligibility', 'contract_renewalFlag', 'benefit_CHIR', 'benefit_CMPL', 'benefit_EPOT', 'benefit_OPT'];
 
-var memberCol = ['COBRA', 'addReason', 'birthDate', 'changeDate', 'displayName', 'duplicateHRN', 'effectiveDate', 'familyHireDate', 'familyPremium', 'familySubscriberId', 'firstName', 'healthRecordNumber', 'individualHealthRecordNumber', 'lastName', 'maritalStatus', 'medicare', 'relationshipDesc', 'relationshipDesc2', 'sex', 'status', 'subgroupId', 'zipcode'];
+
+var memberCol = ['COBRA', 'addReason', 'birthDate', 'changeDate', 'displayName', 'duplicateHRN', 'effectiveDate', 'familyHireDate', 'familyPremium', 'familySubscriberId', 'firstName', 'healthRecordNumber', 'individualHealthRecordNumber', 'lastName', 'maritalStatus', 'medicare','medicarePlanId', 'medicareEntitlementCode', 'relationshipDesc', 'relationshipDesc2', 'sex', 'status', 'subgroupId', 'zipcode'];
 
 var exposeHashToCSVRow = function(cols, obj) {
 	var strRow = '';
@@ -30,9 +31,10 @@ var exposeHashToCSVRow = function(cols, obj) {
 		} else {
 			addedValue = obj[col];
 		}
-		console.log(addedValue);
 		if(typeof(addedValue) =='string' && addedValue.indexOf(',') > -1)
 			addedValue = '\"' + addedValue + '\"';
+		if(typeof(addedValue) =='undefined')
+			addedValue = '';
 		strRow = strRow == '' ? addedValue : strRow + ',' + addedValue;
 	});
 	return strRow;
@@ -40,6 +42,7 @@ var exposeHashToCSVRow = function(cols, obj) {
 
 var processMember = function(member) {
 	var newRow = exposeHashToCSVRow(memberCol, member);
+	newRow = mainGroupId + "," + newRow;
 	if (strMemberJSON != '')
 		strMemberJSON = strMemberJSON + LINE_BREAK;
 	strMemberJSON = strMemberJSON + newRow;
@@ -49,6 +52,7 @@ var processMember = function(member) {
 var processSubGroup = function(subGroup) {
 	var newRow = exposeHashToCSVRow(subGroupCol, subGroup);
 	newRow = mainGroupId + ',' + newRow;
+	mainSubGroupId = subGroup.subgroupId;
 	if (strSubGroupJSON != '')
 		strSubGroupJSON = strSubGroupJSON + LINE_BREAK;
 	strSubGroupJSON = strSubGroupJSON + newRow;
@@ -61,7 +65,7 @@ var processGroup = function(group) {
 	strGroupJSON = strGroupJSON + newRow;
 	mainGroupId = group.groupId;
 	group.subgroups.forEach(processSubGroup);
-	// group.members.forEach(processMember);
+	group.members.forEach(processMember);
 };
 
 
@@ -76,5 +80,5 @@ var convert = function() {
 	data.groups.forEach(processGroup);
 	document.getElementById('groupData').value = strGroupJSON;
 	document.getElementById('subGroupData').value = strSubGroupJSON;
-	// document.getElementById('memberData').value = strMemberJSON;
+	document.getElementById('memberData').value = strMemberJSON;
 }
